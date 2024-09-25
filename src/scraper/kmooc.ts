c852263b0643eb955e61e669b44aecdc82193dc7
곽태욱 <gwak2837@gmail.com>
2024-09-25 16:14:30 +0900

    Update kmooc.ts

--- src/scraper/kmooc.ts
+++ src/scraper/kmooc.ts
@@ -1,9 +1,15 @@
 import * as cheerio from 'cheerio'
 
-const a = await fetch('https://www.kmooc.kr/view/course/detail/10429?tm=20240827111058')
-const aa = await a.text()
-const $ = cheerio.load(aa)
+async function getCoverImageURLFromKMOOC(url: string) {
+  const response = await fetch(url)
+  const html = await response.text()
+  const $ = cheerio.load(html)
 
-const styleAttr = $('#main > div.list > div > ul > li.image > div.card_img').attr('style')
-const imageUrl = styleAttr?.match(/url\(['"]?(.*?)['"]?\)/)?.[1]
-console.log('👀 ~ imageUrl:', imageUrl)
+  const styleAttr = $('#main > div.list > div > ul > li.image > div.card_img').attr('style')
+  const imageUrl = styleAttr?.match(/url\(['"]?(.*?)['"]?\)/)?.[1]
+  return imageUrl
+}
+
+const kmoocURL = 'https://www.kmooc.kr/view/course/detail/10429?tm=20240827111058'
+const coverImageURL = await getCoverImageURLFromKMOOC(kmoocURL)
+console.log('👀 ~ coverImageURL:', coverImageURL)

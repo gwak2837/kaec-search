cbbc22baf7cdc3869fb80d11fddfbbb0ec794e85
곽태욱 <gwak2837@gmail.com>
2024-10-05 13:35:23 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'lms.kmooc.kr' }],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lms.kmooc.kr' },
+      { protocol: 'https', hostname: 'd3njjcbhbojbot.cloudfront.net' },
+    ],
   },
 }
 
